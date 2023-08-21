4a419f0470918ace4bb81823e6fcb1ff0f3cda6f
Jitesh Kumar <jiteshece@gmail.com>
2023-08-22 00:53:35 +0530

    fix(husky): added eslint and husky

---
+++ husky-setup.sh
@@ -0,0 +1,24 @@
+
+npm install husky -D
+npx husky-init
+
+npx husky add .husky/commit-msg 'npx --no-install commitlint --edit'
+
+npm install @commitlint/config-conventional @commitlint/cli -D
+
+# create file
+touch .commitlintrc.json
+
+npm install lint-staged -D
+# add this line to package.json
+# "pre-commit": "lint-staged"
+
+touch .lintstagedrc-prettier
+
+touch .lintstagedrc-eslint
+
+
+#for installing prettier:
+npm install --save-dev --save-exact prettier
+#for installing eslint:
+npm install eslint --save-dev
