bdff907491c27c38466b3ea0a7d57e2ee6868793
Jitesh Kumar <jiteshece@gmail.com>
2023-09-20 00:13:13 +0530

    feat(code): fixed auth controller

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,11 +3,11 @@ date=$(date +%d.%m.%Y)
 echo "checking out to dev branch"
 git checkout dev
 echo "Date"  $date
-echo "Version: " $date 
+echo "Version: " $date
 json -I -f package.json -e "this.version=\"$date\""
 echo "Build complete"
 echo "Pushing to docker hub"
-docker build -t hiijitesh/spam-blocker:$date . 
+docker build -t hiijitesh/spam-blocker:$date .
 docker push hiijitesh/spam-blocker:$date
 echo "start docker-compose"
 docker-compose down && docker-compose up -d
