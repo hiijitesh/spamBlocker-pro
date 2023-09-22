78af84589a254ac90318b544656a2d5f9c16efac
Jitesh Kumar <jiteshece@gmail.com>
2023-09-23 02:16:14 +0530

    fix(code): dcoker changes added

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,13 +1,17 @@
 # npm install -g json
 date=$(date +%d.%m.%Y)
+echo "Building..."
 echo "checking out to dev branch"
 git checkout dev
-echo "Date"  $date
-echo "Version: " $date 
+echo "Version: " $date
+echo "Docker Build Started"
 json -I -f package.json -e "this.version=\"$date\""
-echo "Build complete"
-echo "Pushing to docker hub"
-docker build -t hiijitesh/spam-blocker:$date . 
+echo "Docker Build complete"
+echo "Pushing image to Dockerhub"
+docker build -t hiijitesh/spam-blocker:$date .
 docker push hiijitesh/spam-blocker:$date
+echo "Docker Push complete"
+echo "Deploying the container"
 echo "start docker-compose"
 docker-compose down && docker-compose up -d
+echo "Deployment finished"
